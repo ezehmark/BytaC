62b08e6e566aa6e532990e3604eaa3fa1836c74b
Ezeh Mark <ezehmark@gmail.com>
2025-03-22 02:22:58 +0100

    latest build

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,2 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
-
-config.resolver.assetExts = ['db', 'mp3', 'ttf', 'png', 'jpg', 'jpeg'];
-config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
-
-module.exports = config;
+const { getDefaultConfig } = require('@react-native/metro-config');
+module.exports = getDefaultConfig(__dirname);
