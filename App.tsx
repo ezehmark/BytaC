078531d1d4824b16844bdfe555d830115341a0fa
Ezeh Mark <ezehmark@gmail.com>
2025-03-27 14:54:01 +0100

    latest build

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { useEffect } from "react";
 import { StatusBar } from "react-native";                       import { SafeAreaView } from "react-native-safe-area-context";
 import * as NavigationBar from "expo-navigation-bar";           export default function App() {
 	const handleNav = async()=>{
-	await NavigationBar.setBackgroundColorAsync("#2eda5f");
+	await NavigationBar.setBackgroundColorAsync("#2e4a5f");
 	await NavigationBar.setButtonStyleAsync("light")}
 	useEffect(()=>{
 	handleNav()},[]);
   return (
     <>  
-    <StatusBar barStyle={"light-content"} backgroundColor={"#2eda5f"}/>
+    <StatusBar barStyle={"light-content"} backgroundColor={"#2e4a5f"}/>
     <MyApp/>
     </>
   );
